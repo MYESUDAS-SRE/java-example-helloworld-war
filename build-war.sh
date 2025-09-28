cb03c846f429e0ad3cff29d637b09f8be30aa41b
MYESUDAS-SRE <127671877+MYESUDAS-SRE@users.noreply.github.com>
2025-09-28 11:41:52 -0700

    Update build-war.sh

--- build-war.sh
+++ build-war.sh
@@ -4,9 +4,9 @@ echo "### Create helloworld.war file"
 echo
 echo "Calling jar"
 echo
-jar -cvf helloworld.war *.jsp WEB-INF
+jar -cvf java-example-helloworld-war.war *.jsp WEB-INF
 echo
-echo "Moving helloworld.war to dist/ with option --FORCE"
-mv -f helloworld.war dist/
+echo "Moving java-example-helloworld-war.war to dist/ with option --FORCE"
+mv -f java-example-helloworld-war.war dist/
 echo
 echo "Done"
